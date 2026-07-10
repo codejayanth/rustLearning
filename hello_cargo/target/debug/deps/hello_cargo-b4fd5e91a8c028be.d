7ee4926cadc19b6b05ced90ad88ea100e437d34f
Tejasree <59307555+Jayanth-13@users.noreply.github.com>
2026-07-10 20:11:30 +0200

    added the first cargo project and said hi to rust again

---
+++ hello_cargo/target/debug/deps/hello_cargo-b4fd5e91a8c028be.d
@@ -0,0 +1,5 @@
+/Users/ramajayanth/rustLearning/hello_cargo/target/debug/deps/hello_cargo-b4fd5e91a8c028be.d: src/main.rs
+
+/Users/ramajayanth/rustLearning/hello_cargo/target/debug/deps/hello_cargo-b4fd5e91a8c028be: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/ramajayanth/rustLearning/hello_cargo/target/debug/hello_cargo: /Users/ramajayanth/rustLearning/hello_cargo/src/main.rs
